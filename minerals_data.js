910df0f54e37d53b6255e4bc73368566c98cbb8e
iabaran <abaran@lstyazilim.com>
2026-01-28 18:37:14 +0300

    yeni işlemler demir

---
+++ minerals_data.js
@@ -0,0 +1,25 @@
+const mineralsData = {
+    "Demir": {
+        arabicName: "حديد",
+        ebced: 26,
+        ebcedWithArticle: 57, // el-Hadid (الحديد)
+        atomNumber: 26,
+        isotope: 57, // Demir-57
+        surahNumber: 57, // Hadid Suresi
+        count: 6,
+        locations: [
+            { s: 57, a: 25, w: "الحديد", note: "İndirdik (kozmik köken)" },
+            { s: 18, a: 96, w: "الحديد", note: "Zülkarneyn seti" },
+            { s: 17, a: 50, w: "حديدا", note: "Taş veya demir" },
+            { s: 34, a: 10, w: "الحديد", note: "Davud - demiri yumuşattık" },
+            { s: 22, a: 21, w: "حديد", note: "Demir kamçılar" },
+            { s: 50, a: 22, w: "حديد", note: "Keskin görüş" }
+        ],
+        insights: [
+            { label: "Hadid (حديد) Ebced", value: 26, match: "Atom Numarası" },
+            { label: "el-Hadid (الحديد) Ebced", value: 57, match: "Sure Numarası" },
+            { label: "Demir-57 İzotop", value: 57, match: "Sure Numarası" },
+            { label: "Kur'an'ın Merkezi", value: "114/2", match: "57. Sure" }
+        ]
+    }
+};
